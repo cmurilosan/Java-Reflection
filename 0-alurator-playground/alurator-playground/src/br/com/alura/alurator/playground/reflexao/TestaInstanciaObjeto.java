7bfd82e7dfb126e53f333f0313afe388796e2317
Cassio Murilo <cmurilo.san@gmail.com>
2020-09-09 12:02:01 -0300

    Aula 01 - Começando com Reflection

---
+++ 0-alurator-playground/alurator-playground/src/br/com/alura/alurator/playground/reflexao/TestaInstanciaObjeto.java
@@ -0,0 +1,28 @@
+package br.com.alura.alurator.playground.reflexao;
+
+import br.com.alura.alurator.playground.controle.Controle;
+import br.com.alura.alurator.playground.controle.SubControle;
+
+public class TestaInstanciaObjeto {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		
+		Class<Controle> controleClasse1 = Controle.class;
+		
+		SubControle controle = new SubControle();
+		Class<? extends Controle> controleClasse2 = controle.getClass();
+		
+		Class<?> controleClasse3 = Class.forName("br.com.alura.alurator.playground.controle.Controle");
+		
+		Controle objetoControle = controleClasse1.newInstance();
+		
+		Object outroObjetoControle = controleClasse3.newInstance();
+		
+		System.out.println(objetoControle instanceof Controle);
+		
+		System.out.println(outroObjetoControle instanceof Controle);
+		
+	}
+
+}

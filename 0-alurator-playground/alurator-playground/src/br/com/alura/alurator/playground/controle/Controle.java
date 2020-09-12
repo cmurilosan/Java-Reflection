5f23e6fd71684302622db45ee67c34f53af4ff92
Cassio Murilo <cmurilo.san@gmail.com>
2020-09-12 19:17:02 -0300

    Aula 04_A - Invocação de métodos com parâmetros

--- 0-alurator-playground/alurator-playground/src/br/com/alura/alurator/playground/controle/Controle.java
+++ 0-alurator-playground/alurator-playground/src/br/com/alura/alurator/playground/controle/Controle.java
@@ -15,6 +15,26 @@ public class Controle {
 		return lista;
 	}
 	
+	public void metodoControle2(String p1) {
+        System.out.println("Controle.metodoControle2(String p1)");
+        System.out.println("Parâmetro obtido: " + p1);
+
+    }
+
+    public void metodoControle2(String p1, String p2) {
+        System.out.println("Controle.metodoControle2(String p1, String p2)");
+        System.out.println("Parâmetro obtido p1: " + p1);
+        System.out.println("Parâmetro obtido p2: " + p2);
+
+    }
+
+    public void metodoControle2(String p1, Integer p2) {
+        System.out.println("Controle.metodoControle2(String p1, Integer p2)");
+        System.out.println("Parâmetro obtido p1: " + p1);
+        System.out.println("Parâmetro obtido p2: " + p2);
+
+    }
+	
 	private void metodoControle1() {
 		
 	}

0b667e4e6fd51093b6516acad0c92a3f2d771164
Cassio Murilo <cmurilo.san@gmail.com>
2020-09-17 09:57:52 -0300

    Aula 09 - Inversão de controle e injeção de dependências na prática

--- 0-alurator/alurator/src/br/com/alura/alurator/reflexao/Reflexao.java
+++ 0-alurator/alurator/src/br/com/alura/alurator/reflexao/Reflexao.java
@@ -3,14 +3,26 @@ package br.com.alura.alurator.reflexao;
 public class Reflexao {
 
 	public ManipuladorClasse refleteClasse(String fqn) {
-		try {
-			Class<?> classe = Class.forName(fqn);
-			
-			return new ManipuladorClasse(classe);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
+
+        Class<?> classe = getClasse(fqn);
+
+        return new ManipuladorClasse(classe);
+
+
+}
+
+public Class<?> getClasse(String fqn) {
+
+    try {
+        Class<?> classe = Class.forName(fqn);
+
+        return classe;
+
+    } catch (ClassNotFoundException e) {
+
+        e.printStackTrace();
+        throw new RuntimeException(e);
+    }
 	}
 
 }
